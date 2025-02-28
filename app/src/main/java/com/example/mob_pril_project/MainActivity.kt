aa8af66cf2f1ca7c215903f601fd89f412014525
VlaDMaV <makeev.vlados.9.2005@gmail.com>
2025-03-01 00:45:49 +0300

    Пробное приложение

--- app/src/main/java/com/example/mob_pril_project/MainActivity.kt
+++ app/src/main/java/com/example/mob_pril_project/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.mob_pril_project
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val label = findViewById<TextView>(R.id.label)
+        val userData: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button3)
+        button.setOnClickListener {
+            val text = userData.text.toString().trim()
+            if (text == "Hello World") {
+                Toast.makeText(this, "User enter Hello World", Toast.LENGTH_SHORT).show()
+                } else {
+                label.text = text
+            }
         }
     }
 }
